df89b3ae76952553af3a6620ecb4e1937a9ef5b0
TimoH91 <timothyharris04@gmail.com>
2025-06-18 15:03:23 +0100

    Add project files.

---
+++ AppLanches/Models/Token.cs
@@ -0,0 +1,9 @@
+﻿namespace AppLanches.Models;
+
+public class Token
+{
+    public string? AccessToken { get; set; }
+    public string? TokenType { get; set; }
+    public int? UserId { get; set; }
+    public string? UserName { get; set; }
+}
